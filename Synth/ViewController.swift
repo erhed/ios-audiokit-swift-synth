5027dccf65c4de0b4fa0bf2e08b31680ee536844
erhed <erik.hedrenius@gmail.com>
2020-02-29 09:34:19 +0100

    Add XCode project

---
+++ Synth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Synth
+//
+//  Created by Erik on 2020-02-29.
+//  Copyright © 2020 se.maj7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
